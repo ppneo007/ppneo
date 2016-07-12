52db603a26d52e70ab47be1f9e42058531b8221c
梁栋 <277987724@qq.com>
2016-07-12 16:31:17 +0800

    提交-测试标题
    提交-测试文本

---
+++ test.js
@@ -0,0 +1,13 @@
+var table_ndx = crossfilter(all_data);
+var t_dim_doc = table_ndx.dimension(function (d) {
+        return d.doc;
+    });
+var t_dim_timeline = table_ndx.dimension(function (d) {
+        return d.timeline;
+    });
+var t_dim_type = table_ndx.dimension(function (d) {
+        return d.type;
+    });
+var group_time = t_dim_timeline.group().reduceCount().all();
+var group_doc  = t_dim_doc.group().reduceCount().all();
+var group_type = t_dim_type.group().reduceCount().all();
